e83db9894d24516b2a616aeb04707bf25ce23bd9
sophieeverett <sophieeverett@Sophies-MacBook-Air-5.local>
2016-01-19 12:44:17 +1100

    update2

--- js/index.js
+++ js/index.js
@@ -1,55 +1,58 @@
-
-/*Prevent a form submission with the event.preventDefault() function*/
-
-
 $(document).ready(function() {
 
-/*Use the $.ready() handler to delay your code from executing until all DOM assets have been loaded*/
+
+    $(".readmore").on("click", function (e) {
+            e.preventDefault();
+
+    function slidedown(){
+      $('p').slideDown();
+      $('.readmore').hide();
+    }
+
+    $('.readmore').click(slidedown);
+
+    });
 
 
-/*Select the appropriate DOM elements with CSS selectors upon a user's click using the $.click() handler*/
+    $(".readless").on("click", function (e) {
+            e.preventDefault();
 
-/*If a user clicks "Read More" on the primary column:
-have the text in the <p> tag slide down along with a "Read Less" link in the blog post using $.slideDown()
-and $.show()hide the "Read More" link using $.hide()*/
+    function slideup(){
+      $('.hide').slideUp();
+      $('.readless').hide();
+      $('.readmore').show();
+    }
+
+    $('.readless').click(slideup);
+
+    });
 
 
-function slidedown(){
-  $('p').slideDown();
-  $('.readmore').hide();
-}
+    $(".learnmore").on("click", function (e) {
+            e.preventDefault();
 
-$('.readmore').click(slidedown);
+    function slidedown2(){
+      $('#learnmoretext').slideDown();
+      $('.learnmore').hide();
+      $('.learnless').show();
+    }
 
-/*If a user clicks "Read Less" on the primary column:
-have the <p> slide up and hide the "Read Less" link using $.slideUp() and $.hide()
-show the "Read More" link using $.show()*/
+    $('.learnmore').click(slidedown2);
 
-function slideup(){
-  $('.hide').slideUp();
-  $('.readless').hide();
-  $('.readmore').show();
-}
+    });
 
-$('.readless').click(slideup);
 
-/*Using the same functions as above, if a user clicks the "Learn More" link in the sidebar,
-have the <span> inside that <p> slide down and hide the "Learn More" link using $.slideDown() and $.hide()*/
+    $(".learnless").on("click", function (e) {
+            e.preventDefault();
 
-function slidedown2(){
-  $('#learnmoretext').slideDown();
-  $('.learnmore').hide();
-  $('.learnless').show();
-}
+    function slideup2(){
+      $('.hide').slideUp();
+      $('.learnless').hide();
+      $('.learnmore').show();
+    }
 
-$('.learnmore').click(slidedown2);
+    $('.learnless').click(slideup2);
 
-function slideup2(){
-  $('.hide').slideUp();
-  $('.learnless').hide();
-  $('.learnmore').show();
-}
-
-$('.learnless').click(slideup2);
+    });
 
 });
